a2dfe017daaa4a5e8e5cf1d24bbffc3fc3cd7da7
raziel7893 <raziel7893@gmail.com>
2026-03-15 14:16:44 +0100

    fix empyrion

--- WindowsGSM/Functions/ProcessManagement.cs
+++ WindowsGSM/Functions/ProcessManagement.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Management;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace WindowsGSM.Functions
 {
     static class ProcessManagement
     {
+        internal const int CTRL_C_EVENT = 0;
+        [DllImport("kernel32.dll")]
+        internal static extern bool GenerateConsoleCtrlEvent(uint dwCtrlEvent, uint dwProcessGroupId);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool AttachConsole(uint dwProcessId);
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        internal static extern bool FreeConsole();
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleCtrlHandler(ConsoleCtrlDelegate HandlerRoutine, bool Add);
+        delegate Boolean ConsoleCtrlDelegate(uint CtrlType);
+
+        public static bool SendStopSignal(Process p)
+        {
+            if (AttachConsole((uint)p.Id))
+            {
+                SetConsoleCtrlHandler(null, true);
+                try
+                {
+                    if (!GenerateConsoleCtrlEvent(CTRL_C_EVENT, 0))
+                        return false;
+                    p.WaitForExit(10000);
+                }
+                finally
+                {
+                    SetConsoleCtrlHandler(null, false);
+                    FreeConsole();
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public static void StopProcess(Process p)
+        {
+            if (!SendStopSignal(p))
+                p.Kill();
+        }
+
         public static async Task<string> GetCommandLineByApproximatePath(string path)
         {
             return await Task.Run(() =>
